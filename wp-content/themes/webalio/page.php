df0e8d54447a4a075d05fab0f455efb357fc5933
almazka <almazka@flylady.su>
2016-08-03 11:59:22 +0600

    minor changes

---
+++ wp-content/themes/webalio/page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : ?>
+		<div class="content">
+			 <?php while ( have_posts() ) : the_post(); ?>
+					<?php the_content(); ?>
+			 <?php endwhile; ?>
+		</div>
+<?php endif; ?>
+
+<?php get_footer(); ?>
